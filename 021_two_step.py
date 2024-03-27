d139313f860e63f267ffda2466dbfa5e41defaec
mickeymarse <mmserrapica@tutanota.com>
2024-03-27 11:22:55 +0000

    021 completed

--- 021_two_step.py
+++ 021_two_step.py
@@ -38,8 +38,9 @@ print("")
 print("Function: divide_by_two_and_add_one")
 
 def divide_by_two_and_add_one(num):
-  # Divide num by two and add one to the result
-  pass # <-- This does nothing, replace it with your code
+  halved = num / 2
+  added = halved + 1
+  return added
 
 check_that_these_are_equal(
   divide_by_two_and_add_one(6),
@@ -52,8 +53,9 @@ print("")
 print("Function: multiply_by_forty_and_add_sixty")
 
 def multiply_by_forty_and_add_sixty(num):
-  # Multiply num by forty, and then add sixty
-  pass # <-- This does nothing, replace it with your code
+  forty = num * 40
+  sixty = forty + 60
+  return sixty
 
 check_that_these_are_equal(
   multiply_by_forty_and_add_sixty(3423),
@@ -66,8 +68,9 @@ print("")
 print("Function: add_together_and_double")
 
 def add_together_and_double(num_a, num_b):
-  # Add together num_a and num_b, then double the result
-  pass # <-- This does nothing, replace it with your code
+  added = num_a + num_b
+  doubled = added * 2
+  return doubled
 
 check_that_these_are_equal(
   add_together_and_double(3, 4),
